c8d9ce15651d80000b1eda9dc3926c23d6156e84
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-04-24 13:07:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,6 @@ const themeLabel = document.querySelector(".theme-label");
 const themeIcon = document.querySelector(".theme-icon");
 
 const body = document.querySelector("body")
-// const logoText = document.querySelector(".logo-text")
-// const searchContainer = document.querySelector(".search")
-// const mainCard = document.querySelector(".main-body")
-
-// const statsSection = document.querySelector(".stats-section");
-// const githubCount = document.querySelectorAll(".github-count");
-// const githubStats = document.querySelectorAll(".repos, .followers, .following")
-
-// const linkItems = document.querySelectorAll(".link")
 
 function updateThemeButtonContent(isLightTheme) {
     if (isLightTheme) {
@@ -24,26 +15,7 @@ function updateThemeButtonContent(isLightTheme) {
     }
 }
 
-// function toggleClass(element, className) {
-//     return element.classList.toggle(className);
-// }
-
 function applyThemeStyles() {
-    //  themeElements.forEach(([element, className]) => {
-    //     toggleClass(element, className);
-    // });
-
-
-    // githubStats.forEach(stat => {
-    //     toggleClass(stat, "text-light-primary")
-    // })
-    // githubCount.forEach(count => {
-    //     toggleClass(count, "text-light-secondary")
-    // })
-
-    // linkItems.forEach(link => {
-    //     toggleClass(link, "text-light-primary")
-    // })
 
     body.classList.toggle("light-theme")
 
@@ -77,17 +49,6 @@ function initTheme() {
 }
 
 initTheme();
-
-// const themeElements = [
-//     [body, "bg-light-primary"],
-//     [logoText, "text-light-tertiary"],
-//     [searchContainer, "bg-light-secondary"],
-//     [mainCard, "bg-light-secondary"],
-//     [userName, "text-light-secondary"],
-//     [userDate, "text-light-primary"],
-//     [bioText, "text-light-primary"],
-//     [statsSection,"bg-light-primary"]
-// ];
 
 switchMode.addEventListener("click", () => {
     
@@ -180,10 +141,3 @@ searchForm.addEventListener("submit", async (e) => {
         errorContainer.classList.remove("hidden");
     }
 })
-
-// inputId.addEventListener("keydown", (e) => {
-//     if(e.key === "Enter") {
-//         inputBtn.click();
-//     }
-// })
-
